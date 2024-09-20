4e6d46bc739c1c91218687fd19283f0251a30dd8
Arif-Siregar <150424609+Arif-Siregar@users.noreply.github.com>
2024-09-20 10:29:49 +1000

    Added create function

--- PokemonReviewApp/Controllers/ReviewController.cs
+++ PokemonReviewApp/Controllers/ReviewController.cs
@@ -13,11 +13,16 @@ namespace PokemonReviewApp.Controllers
     {
         private readonly IReviewRepository _reviewRepository;
         private readonly IMapper _mapper;
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IPokemonRepository _pokemonRepository;
 
-        public ReviewController(IReviewRepository reviewRepository, IMapper mapper) 
+        public ReviewController(IReviewRepository reviewRepository, IMapper mapper, 
+            IReviewerRepository reviewerRepository, IPokemonRepository pokemonRepository) 
         {
             _reviewRepository = reviewRepository;
             _mapper = mapper;
+            _reviewerRepository = reviewerRepository;
+            _pokemonRepository = pokemonRepository;
         }
 
         [HttpGet]
@@ -59,6 +64,43 @@ namespace PokemonReviewApp.Controllers
                 return BadRequest(ModelState);
 
             return Ok(reviews);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateReview([FromQuery] int reviewerId, [FromQuery] int pokeId, [FromBody] ReviewDto reviewCreate)
+        {
+            if (reviewCreate == null)
+                return BadRequest(ModelState);
+
+            var review = _reviewRepository.GetReviews()
+                .Where(c => c.Title.Trim().ToUpper() == reviewCreate.Title.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (review != null)
+            {
+                ModelState.AddModelError("", "Review already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewMap = _mapper.Map<Review>(reviewCreate);
+
+            reviewMap.Reviewer = _reviewerRepository.GetReviewer(reviewerId);
+            reviewMap.Pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if (!_reviewRepository.CreateReview(reviewMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+
+            return (Ok("Sucessfully created"));
+
+
         }
     }
 }
--- PokemonReviewApp/Repository/CategoryRepository.cs
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -16,6 +16,14 @@ namespace PokemonReviewApp.Repository
             return _context.Categories.Any(c => c.Id == id);
         }
 
+        //Change Tracker - add, update, modify
+        //Most of the time connected, sometimes disconnected (EntityState.Added)
+        public bool CreateCategory(Category category)
+        {
+            _context.Add(category);
+            return Save();
+        }
+
         public ICollection<Category> GetCategories()
         {
             return _context.Categories.ToList();
@@ -29,6 +37,18 @@ namespace PokemonReviewApp.Repository
         public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
         {
             return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(e => e.Pokemon).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            _context.Update(category);
+            return Save();
         }
     }
 }
